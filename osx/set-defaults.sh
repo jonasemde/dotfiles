40712d446277e68724dbd4b37bd3eba3237116f1
Jonas Emde <emde@mindtwo.de>
2019-01-03 00:09:28 +0100

    Update from freek

--- osx/set-defaults.sh
+++ osx/set-defaults.sh
@@ -4,6 +4,9 @@ echo 'start osx/set-defaults.sh'
 # Ask for the administrator password upfront
 sudo -v
 
+# Keep-alive: update existing `sudo` time stamp until `.osx` has finished
+while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
+
 # Set computer name (as done via System Preferences → Sharing)
 sudo scutil --set ComputerName "Jonas MBP"
 sudo scutil --set HostName "jonas-mbp"
@@ -14,7 +17,6 @@ sudo dscacheutil -flushcache
 ###############################################################################
 # General UI/UX                                                               #
 ###############################################################################
-
 # Set sidebar icon size to medium
 defaults write NSGlobalDomain NSTableViewDefaultSizeMode -int 2
 
